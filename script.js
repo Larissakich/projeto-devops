d352b3a4344a0ce7cab78a315369debdca225466
Larissa Kich <laryssakich@gmail.com>
2026-03-23 21:19:26 -0300

    Testando pipeline

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function mensagem() {
-  alert("Pipeline funcionando 🚀");
+  const toast = document.getElementById('toast');
+  toast.classList.add('show');
+  setTimeout(() => toast.classList.remove('show'), 3000);
 }
